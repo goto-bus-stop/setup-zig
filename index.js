1554e6075541868810520ea0a1a712f923c1d394
Renée Kooi <renee@kooi.me>
2020-01-30 15:11:52 +0100

    fix lint

--- index.js
+++ index.js
@@ -41,11 +41,6 @@ async function downloadZig (version) {
     throw new Error(`Could not find version ${version} for platform ${host}`)
   }
 
-  const hostVariantName = {
-    linux: 'linux-x86_64',
-    darwin: 'macos-x86_64',
-    win32: 'windows-x86_64'
-  }[os.platform()]
   const variantName = path.basename(meta[host].tarball).replace(`.${ext}`, '')
   const downloadPath = await cache.downloadTool(meta[host].tarball)
   const zigPath = ext === 'zip'
