513661dd8994a0c10e68367ce33df5a8048d0011
Renée Kooi <renee@kooi.me>
2020-02-01 17:09:24 +0100

    siiiiiigh

--- index.js
+++ index.js
@@ -50,14 +50,6 @@ async function downloadZig (version) {
     : await cache.extractTar(downloadPath, undefined, 'x')
 
   const binPath = path.join(zigPath, variantName)
-
-  console.log({
-    variantName,
-    downloadPath,
-    zigPath,
-    binPath,
-  })
-
   const cachePath = await cache.cacheDir(binPath, 'zig', variantName)
 
   return cachePath
