d485d46e1571b7b541c286f0dc770c33c4319edf
Renée Kooi <renee@kooi.me>
2020-11-11 12:38:41 +0100

    add tests for version to URL resolution

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
 const os = require('os')
 const path = require('path')
 const semver = require('semver')
-const get = require('simple-get').concat
 const actions = require('@actions/core')
 const cache = require('@actions/tool-cache')
-
-function getJSON (opts) {
-  return new Promise((resolve, reject) => {
-    get({ ...opts, json: true }, (err, req, data) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
-
-function extForPlatform (platform) {
-  return {
-    linux: 'tar.xz',
-    darwin: 'tar.xz',
-    win32: 'zip'
-  }[platform]
-}
-
-function resolveCommit (platform, version) {
-  const ext = extForPlatform(platform)
-  const addrhost = {
-    linux: 'linux-x86_64',
-    darwin: 'macos-x86_64',
-    win32: 'windows-x86_64'
-  }[platform]
-
-  const downloadUrl = `https://ziglang.org/builds/zig-${addrhost}-${version}.${ext}`
-  const variantName = `zig-${addrhost}-${version}`
-
-  return { downloadUrl, variantName }
-}
-
-async function resolveVersion (platform, version) {
-  const ext = extForPlatform(platform)
-  const host = {
-    linux: 'x86_64-linux',
-    darwin: 'x86_64-macos',
-    win32: 'x86_64-windows'
-  }[platform] || platform
-
-  const index = await getJSON({ url: 'https://ziglang.org/download/index.json' })
-
-  const availableVersions = Object.keys(index)
-  const useVersion = semver.valid(version)
-    ? semver.maxSatisfying(availableVersions.filter((v) => semver.valid(v)), version)
-    : null
-
-  const meta = index[useVersion || version]
-  if (!meta || !meta[host]) {
-    throw new Error(`Could not find version ${version} for platform ${host}`)
-  }
-
-  const downloadUrl = meta[host].tarball
-  const variantName = path.basename(meta[host].tarball).replace(`.${ext}`, '')
-
-  return { downloadUrl, variantName }
-}
+const {
+  extForPlatform,
+  resolveCommit,
+  resolveVersion
+} = require('./versions')
 
 async function downloadZig (platform, version) {
   const ext = extForPlatform(platform)
---
+++ versions.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const get = require('simple-get').concat
+const semver = require('semver')
+
+function extForPlatform (platform) {
+  return {
+    linux: 'tar.xz',
+    darwin: 'tar.xz',
+    win32: 'zip'
+  }[platform]
+}
+
+function resolveCommit (platform, version) {
+  const ext = extForPlatform(platform)
+  const addrhost = {
+    linux: 'linux-x86_64',
+    darwin: 'macos-x86_64',
+    win32: 'windows-x86_64'
+  }[platform]
+
+  const downloadUrl = `https://ziglang.org/builds/zig-${addrhost}-${version}.${ext}`
+  const variantName = `zig-${addrhost}-${version}`
+
+  return { downloadUrl, variantName }
+}
+
+function getJSON (opts) {
+  return new Promise((resolve, reject) => {
+    get({ ...opts, json: true }, (err, req, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+async function resolveVersion (platform, version) {
+  const ext = extForPlatform(platform)
+  const host = {
+    linux: 'x86_64-linux',
+    darwin: 'x86_64-macos',
+    win32: 'x86_64-windows'
+  }[platform] || platform
+
+  const index = await getJSON({ url: 'https://ziglang.org/download/index.json' })
+
+  const availableVersions = Object.keys(index)
+  const useVersion = semver.valid(version)
+    ? semver.maxSatisfying(availableVersions.filter((v) => semver.valid(v)), version)
+    : null
+
+  const meta = index[useVersion || version]
+  if (!meta || !meta[host]) {
+    throw new Error(`Could not find version ${useVersion || version} for platform ${host}`)
+  }
+
+  const downloadUrl = meta[host].tarball
+  const variantName = path.basename(meta[host].tarball).replace(`.${ext}`, '')
+
+  return { downloadUrl, variantName }
+}
+
+module.exports = {
+  extForPlatform,
+  resolveCommit,
+  resolveVersion
+}
